685c56f96c035fae27bec1c237922dc544a09eda
Hayk <hajko92@mail.ru>
2021-08-11 13:04:14 +0400

    CSVReaderTest2 added

--- src/test/java/audioVisualizator/util/CSVReaderTest.java
+++ src/test/java/audioVisualizator/util/CSVReaderTest.java
@@ -9,8 +9,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CSVReaderTest {
     @Test
-    public void getDataFromFIleTest() {
-        File file = new File("Wrong path");
+    public void getDataFromFIleTest1() {
+        File file = new File("wrong path");
         assertThrows(AppRuntimeException.class, () -> CSVReader.getDataFromFIle(file));
+    }
+
+    @Test
+    public void getDataFromFIleTest2() {
+        File file = new File("C:\\Users\\Hayk\\IdeaProjects\\AudioVisualizator-\\src\\main\\resources\\item.csv");
+        String lineFromFile = "1,100,Test1,https://test.test/image/1,1";
+        String lineFromReader = CSVReader.getDataFromFIle(file).get(0);
+        assertEquals(lineFromFile, lineFromReader);
     }
 }
